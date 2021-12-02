180366fba13f9277bcebcdcde57629a952234f10
Phillip Harrison <philharrisongtr@gmail.com>
2021-12-02 14:29:00 +0000

    further development of app layout components

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   components: {
     AppContainer,
     AppHeader,
-    AppSideBar
+    AppSideBar,
 },
 }
 
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+          </div>
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped>
+.col-sm {
+    border: solid 1px black;
+    
+}
+
+
+</style>
