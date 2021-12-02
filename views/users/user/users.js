49711f4a144f0578219f3f27ef03bfc6e4696d34
Phillip Harrison <philharrisongtr@gmail.com>
2021-12-02 11:15:02 +0000

    2_12_prerouter

---
+++ views/users/user/users.js
@@ -0,0 +1,54 @@
+const users = [{
+        id: 1,
+        username: 'John',
+        age: 31,
+        occupation: 'dentist'
+    },
+    {
+        id: 2,
+        username: 'Tom',
+        age: 33,
+        occupation: 'tennis player'
+    },
+    {
+        id: 3,
+        username: 'Eric',
+        age: 37,
+        occupation: 'golfer'
+    }, {
+        id: 4,
+        username: 'Steve',
+        age: 41,
+        occupation: 'footballer'
+    },
+    {
+        id: 5,
+        username: 'Trevor',
+        age: 66,
+        occupation: 'bartender'
+    },
+    {
+        id: 6,
+        username: 'Dean',
+        age: 31,
+        occupation: 'singer'
+    },
+    {
+        id: 7,
+        username: 'Roger',
+        age: 26,
+        occupation: 'physiotherapist'
+    },
+    {
+        id: 8,
+        username: 'Tony',
+        age: 28,
+        occupation: 'retired'
+    },
+    {
+        id: 9,
+        username: 'John',
+        age: 49,
+        occupation: 'dentist'
+    }
+]
